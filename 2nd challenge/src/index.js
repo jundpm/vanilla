aa20c937f8c3dc368190c60b9de65670f890fe29
jundpm <60919492+jundpm@users.noreply.github.com>
2023-11-03 21:02:11 +0900

    second challenge finished

--- 2nd challenge/src/index.js
+++ 2nd challenge/src/index.js
@@ -1,22 +1,26 @@
 const heightOutput = document.querySelector("#height");
 const widthOutput = document.querySelector("#width");
 
+const body = document.querySelector("body");
+
+const h2 = document.querySelector("h2");
+h2.style.color = "white";
+console.dir(body);
+
 function updateSize() {
   //heightOutput.textContent = window.innerHeight;
-  widthOutput.textContent = window.innerWidth;
+  //widthOutput.textContent = window.innerWidth;
+  if (window.innerWidth < 600) {
+    body.style.background = "blue";
+  } else if (window.innerWidth < 1000) {
+    body.style.background = "purple";
+  } else {
+    body.style.background = "yellow";
+  }
 }
 
 updateSize();
 window.addEventListener("resize", updateSize);
 
-function widthColor() {
-    if (window.innerWidth >1100) {
-        body.style.backgroundcolor = "red";
-    } else {
-        
-    }
-}
 
 
-
-window.addEventListener("innerWidth", widthColor);
