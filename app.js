8a2cc0ef8971b0c9aadbabf5bb476777171b06b7
jundpm <60919492+jundpm@users.noreply.github.com>
2023-11-03 15:31:33 +0900

    lecture 3 finished

--- app.js
+++ app.js
@@ -1,44 +1,8 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    //console.log("title was clicked!");
-    h1.style.color = "red";
+    const clickedClass = "clicked"
+    h1.classList.toggle("clicked");
 }
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-
-}
-
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-window.addEventListener("resize",handleWindowResize);
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-window.addEventListener("copy", handleWindowCopy);
-
-
-function handleWindowOffline() {
-    alert("SoS no Wifi");
-}
-
-window.addEventListener("offline", handleWindowOffline);
-
-function handleWindowOnline() {
-    alert("all good");
-}
-
-window.addEventListener("online", handleWindowOnline);
